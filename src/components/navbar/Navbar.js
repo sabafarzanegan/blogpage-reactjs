8884b7e2306bb06fa12e979869a163874eaf0bb2
saba farzanegan <saba.far.99@gmail.com>
2024-04-07 16:55:14 +0330

    creating dark mood

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Navbar() {
   const [isShowBar, setisShowBar] = useState(false);
- 
+  const [theme, setTheme] = useState(false);
 
   const showbarhandler = () => {
     setisShowBar((prev) => !prev);
@@ -11,10 +11,28 @@ function Navbar() {
   const hiddenbarhandler = () => {
     setisShowBar(false);
   };
- 
+  const lighthandlertoggle = () => {
+    setTheme(false);
+  
+    document.querySelector("html").classList.remove("dark");
+     document.querySelector("body").classList.remove("bg-dark");
+   
+  };
+  const darkhandlertoggle = () => {
+    setTheme(true);
+    
+    document.querySelector("html").classList.add("dark");
+    document.querySelector("body").classList.add("bg-dark")
+    
+  };
+  useEffect(() => {
+    darkhandlertoggle();
+    lighthandlertoggle();
+  }, []);
+
   return (
     <>
-      <div className="w-full h-16 bg-white shadow-lg fixed relative top-0 right-0 z-10 p-2 font-vazir dark:bg-orange-600 ">
+      <div className="w-full h-16 bg-white shadow-lg fixed relative top-0 right-0 z-10 p-2 font-vazir dark:bg-gray-800 dark:text-zinc-300 ">
         <div className="flex justify-between items-center w-full h-full p-6 mt-1">
           <div className="flex items-center justify-center gap-5">
             <img
@@ -23,36 +41,38 @@ function Navbar() {
               alt=""
             />
             <div className="flex items-center justify-center gap-3 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-                
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-               
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-                />
-              </svg>
+              <button onClick={lighthandlertoggle}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={`w-6 h-6  `}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                  />
+                </svg>
+              </button>
+              <button onClick={darkhandlertoggle}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={`w-6 h-6 `}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+                  />
+                </svg>
+              </button>
             </div>
           </div>
 
